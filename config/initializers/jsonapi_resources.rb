757e0ef27b748649fc7b62af1c623084c7e7fff5
lgebhardt <larry@cerebris.com>
2024-01-29 12:30:46 -0500

    Update JR Config

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -1,4 +1,8 @@
 JSONAPI.configure do |config|
+  # Set the retrieval strategy in the resource base classes to allow testing of different strategies. So we will set
+  # the default to :none to prevent trying to load two competing modules
+  config.default_resource_retrieval_strategy = :none
+
   config.resource_key_type = :uuid
 
   if Rails.application.config.action_controller.perform_caching
@@ -7,15 +11,9 @@ JSONAPI.configure do |config|
   end
 
   # Options are :none, :offset, :paged, or a custom paginator name
-  config.default_paginator = :paged # default is :none
-
-  # config.processor_klass = JSONAPI::AuthorizingProcessor
-
+  config.default_paginator = :paged
   config.default_page_size = 10
   config.maximum_page_size = 2000
-  config.default_resource_retrieval_strategy = :none
 
-
-  config.default_find_related_through = :inverse
-  config.default_find_related_through_polymorphic = :inverse
+  # config.related_identities_set = SortedSet
 end
